54a03553fc5e5104f33cec7a80d036677d6c8bf0
Akash6222 <akashdubey25274@gmail.com>
2023-12-06 03:17:38 +0530

    [REM] dropdown section from telescope

--- lua/akash/plugins/nvim-telescope.lua
+++ lua/akash/plugins/nvim-telescope.lua
@@ -176,53 +176,6 @@ function M.config()
         },
       },
     },
-    pickers = {
-      live_grep = {
-        theme = "dropdown",
-      },
-      grep_string = {
-        theme = "dropdown",
-      },
-      find_files = {
-        theme = "dropdown",
-        previewer = false,
-      },
-      buffers = {
-        theme = "dropdown",
-        previewer = false,
-        initial_mode = "normal",
-      },
-      planets = {
-        show_pluto = true,
-        show_moon = true,
-      },
-      colorscheme = {
-        -- enable_preview = true,
-      },
-      lsp_references = {
-        theme = "dropdown",
-        initial_mode = "normal",
-      },
-      lsp_definitions = {
-        theme = "dropdown",
-        initial_mode = "normal",
-      },
-      lsp_declarations = {
-        theme = "dropdown",
-        initial_mode = "normal",
-      },
-      lsp_implementations = {
-        theme = "dropdown",
-        initial_mode = "normal",
-      },
-      -- Default configuration for builtin pickers goes here:
-      -- picker_name = {
-      --   picker_config_key = value,
-      --   ...
-      -- }
-      -- Now the picker_config_key will be applied every time you call this
-      -- builtin picker
-    },
     extensions = {
       file_browser = {
         theme = "ivy",
