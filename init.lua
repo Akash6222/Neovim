d77a4227549d15ff61e12e87a8f375649ba6ed07
Akash6222 <akashdubey25274@gmail.com>
2024-07-01 01:30:12 +0530

    Updated nvim
    It's working fine currently, expect few minor issues

--- init.lua
+++ init.lua
@@ -1,23 +1,14 @@
 vim.loader.enable()
 
-require("akash.options")
-require("akash.keymaps")
-require("akash.lazy")
-require("akash.autocommands")
---require("akash.macros")
-require("akash.icons")
--- require("akash.which-key")
-
--- Enable autochdir
-vim.cmd([[set autochdir]])
-
--- Enable autochdir
-vim.o.autochdir = true
+require("rj.options")
+require("rj.lazy")
+require("rj.autocommands")
+require("rj.keymaps")
 
 -- Bufferline 
 require("bufferline").setup{}
-require("octo").setup{}
 
 -- Discord 
 require("presence").setup{}
+
 
---
+++ lua/rj/lsp/attach.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded",
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = "rounded",
+})
+
+local function attach_navic(client, bufnr)
+  vim.g.navic_silence = true
+  local navic = require("nvim-navic")
+  navic.attach(client, bufnr)
+end
+
+local function lsp_keymaps()
+  local keymap = vim.keymap.set
+  local opts = { noremap = true, silent = true, buffer = true }
+  keymap("n", "gD", "<cmd>Telescope lsp_document_symbols<CR>", opts)
+  keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+  keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  keymap("n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts)
+  keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  keymap("n", "<C-n>", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
+  keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+  keymap("n", "gR", '<cmd>lua require("trouble").toggle("lsp_references")<CR>', opts)
+  keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  keymap("n", "<leader>Q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+end
+
+M.on_attach = function(client, bufnr)
+  attach_navic(client, bufnr)
+  lsp_keymaps()
+  if vim.fn.has("nvim-0.10") == 1 and vim.bo.filetype ~= "rust" then
+    vim.lsp.inlay_hint.enable(0, true)
+  end
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
+return M
